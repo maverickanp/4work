17d3805c1fd575f45f6f02118aa5124ab6cc8e30
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2018-04-18 10:55:12 -0300

    Commit inicial

---
+++ app/src/main/java/com/forwork/app/activities/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.forwork.app.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.forwork.app.R;
+
+public class SignInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
